f3881af0c61b92760088bc36bd894c87d59b20ae
David <davido262@gmail.com>
2012-11-06 11:10:40 -0600

    added a test component

---
+++ include/testcomponent.hpp
@@ -0,0 +1,53 @@
+/*
+ *    Copyright (c) 2012 David Cavazos <davido262@gmail.com>
+ *
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *    OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#ifndef TESTCOMPONENT_HPP
+#define TESTCOMPONENT_HPP
+
+#include "engine/kernel/component.hpp"
+
+const std::string COMPONENT_TESTCOMPONENT = "testcomponent";
+
+class TestComponent: public Component {
+public:
+    TestComponent(Entity* const entity);
+    ~TestComponent();
+
+    double getHealth() const;
+
+private:
+    double m_health;
+
+    void cmdHealth(const std::string& arg);
+};
+
+
+
+inline double TestComponent::getHealth() const {
+    return m_health;
+}
+
+#endif // TESTCOMPONENT_HPP
